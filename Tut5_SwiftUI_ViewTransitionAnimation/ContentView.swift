7a91f3b5dceb4a45bdebe44b9f27d62fee023a23
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-26 02:02:52 +0600

    Assymetric Transition

--- Tut5_SwiftUI_ViewTransitionAnimation/ContentView.swift
+++ Tut5_SwiftUI_ViewTransitionAnimation/ContentView.swift
@@ -38,6 +38,13 @@ struct ContentView: View {
                             .bold()
                             .foregroundColor(.white)
                     )
+                
+                
+            //MARK: - Symmetric Transition
+                /**
+                 Below is the examples of symmetrical transition. Uncomment one by one to see the result
+                 */
+                
                 //Scale Transition with anchor set to bottom
                     //.transition(.scale(scale: 0, anchor: .bottom))
                 
@@ -49,7 +56,15 @@ struct ContentView: View {
                     //.transition(.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity))
                 
                 //Reusable Animation
-                    .transition(.offsetScaleOpacity)
+                    //.transition(.offsetScaleOpacity)
+                
+            //MARK: - Symmetric Transition
+                    /**
+                     Below is the examples of Asymmetrical transition. Uncomment to see the result
+                     */
+//                    .transition(.asymmetric(insertion: .scale(scale: 0, anchor: .bottom), removal: .offset(x: -600,y: 0)))
+                //Reusable Animation
+                    .transition(.scaleAndOffset)
                 
             }
         }
@@ -68,5 +83,9 @@ struct ContentView_Previews: PreviewProvider {
 extension AnyTransition{
     static var offsetScaleOpacity: AnyTransition{
         AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity)
+    }
+    
+    static var scaleAndOffset: AnyTransition{
+        AnyTransition.asymmetric(insertion: .scale(scale: 0, anchor: .bottom), removal: .offset(x: -600, y: 0))
     }
 }
