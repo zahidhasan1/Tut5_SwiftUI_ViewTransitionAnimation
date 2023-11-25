707b88b208281523c47bf12dbbb2b52636cf9cfc
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-26 01:34:15 +0600

    Initial Commit

---
+++ Tut5_SwiftUI_ViewTransitionAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tut5_SwiftUI_ViewTransitionAnimation
+//
+//  Created by ZEUS on 26/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tut5_SwiftUI_ViewTransitionAnimation/Tut5_SwiftUI_ViewTransitionAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut5_SwiftUI_ViewTransitionAnimationApp.swift
+//  Tut5_SwiftUI_ViewTransitionAnimation
+//
+//  Created by ZEUS on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tut5_SwiftUI_ViewTransitionAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
